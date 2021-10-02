2467ac417f3a76c329c5169b03c757c8311846cf
Theo Petit <theo.p83@gmail.com>
2021-10-02 15:16:11 +0200

    problem 2

--- problems.py
+++ problems.py
@@ -4,3 +4,15 @@ def solve1():
         if i % 3 == 0 or i % 5 == 0:
             res += i
     print(res)
+
+def solve2():
+    res = 0
+    n = 1
+    m = 1
+    while n <= 4000000:
+        mem = n
+        n = m
+        m = mem + n
+        if n % 2 == 0:
+            res += n
+    print(res)
